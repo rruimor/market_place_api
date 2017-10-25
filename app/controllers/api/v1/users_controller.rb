c223437ef371281b1f5f99da513bbfcc02fff0d0
Rafael Ruiz <raf.ruiz.moreno@gmail.com>
2017-10-25 15:50:20 +0200

    Adds #update action to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,8 +7,19 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+
     if user.save
       render json: user, status: 201
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: user, status: 200, location: [:api, user]
     else
       render json: { errors: user.errors }, status: 422
     end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,25 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
+
   before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
 
   describe "GET #show" do
-      before(:each) do
-        @user = FactoryBot.create :user
-        get :show, params: { id: @user.id }, format: :json
-      end
-  
-      it "returns the information about a reporter on a hash" do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:email]).to eql @user.email
-      end
-  
-      it { should respond_with 200 }
+
+    before(:each) do
+      @user = FactoryBot.create :user
+      get :show, params: { id: @user.id }, format: :json
+    end
+
+    it "returns the information about a reporter on a hash" do
+      user_response = JSON.parse(response.body, symbolize_names: true)
+      expect(user_response[:email]).to eql @user.email
+    end
+
+    it { should respond_with 200 }
+
   end
 
   describe "POST #create" do
     
     context "when is successfully created" do
+
       before(:each) do
         @user_attributes = FactoryBot.attributes_for :user
         post :create, params: { user: @user_attributes }, format: :json
@@ -31,9 +35,11 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       end
 
       it { should respond_with 201 }
+
     end
 
     context "when is not created" do
+
       before(:each) do
         @invalid_user_attributes = { 
           password: "123123123", 
@@ -53,8 +59,58 @@ RSpec.describe Api::V1::UsersController, type: :controller do
         expect(user_response[:errors][:email]).to include "can't be blank"
       end
 
-      it { should respond_with 422 }      
+      it { should respond_with 422 }
+
     end
 
   end
+
+  describe "PUT/PATCH #update" do
+
+    context "when it is successfully updated" do
+
+      before(:each) do
+        @user = FactoryBot.create :user
+        update_params = { 
+          id: @user.id, 
+          user: { email: "newmail@a.com" } 
+        }
+        patch :update, params: update_params, format: :json
+      end
+
+      it "renders the JSON representation for the updated user" do
+
+      end
+
+      it { should respond_with 200 }
+
+    end
+
+    context "when it is not created" do
+
+      before(:each) do
+        @user = FactoryBot.create :user
+        invalid_update_params = { 
+          id: @user.id, 
+          user: { email: "bademail.com" }
+        }
+        patch :update, params: invalid_update_params, format: :json
+      end
+
+      it "renders an errors JSON" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the JSON errors on why the users could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "is invalid"
+      end
+
+      it { should respond_with 422 }
+
+    end
+
+  end
+
 end
